766154b00cf6048c6edf8fe14631c9fbb1576127
Niko Neugebauer <info@webcaravela.com>
2016-11-07 14:15:15 -0800

    0.2.0 MOSL Update Correction

--- update-mosl.ps1
+++ update-mosl.ps1
@@ -1,6 +1,6 @@
-﻿#	CISL - Columnstore Indexes Scripts Library for SQL Server
-#	Powershell Script to install the CISL in the user databases
-#	Version: 1.4.1, November 2016
+﻿#	MOSL - Memory Optimised Scripts Library for SQL Server
+#	Powershell Script to install the MOSL in the user databases
+#	Version: 0.2.0, November 2016
 #
 #	Copyright 2015-2016 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)
 #
@@ -19,27 +19,27 @@
 #    See the License for the specific lan guage governing permissions and
 #    limitations under the License.
 
-function Update-CISL(
+function Update-mosl(
     [parameter(Mandatory=$true)]
-    $path = 'C:\Work\CISL'
+    $path = 'C:\Work\mosl'
 )
 {
     # Writing the header
-    Write-Host "CISL - Columnstore Indexes Scripts Library for SQL Server: " -ForegroundColor Green
-    Write-Host "Version: 1.4.1, November 2016" -ForegroundColor Green
+    Write-Host "MOSL - Memory Optimised Scripts Library for SQL Server: " -ForegroundColor Green
+    Write-Host "Version: 0.2.0, November 2016" -ForegroundColor Green
     Write-Host "(c) 2015-2016 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)" -ForegroundColor Green
     Write-Host "----------------------------------------------------------------------------------------------------"
     
         
-    if (!(Test-Path -Path "$tempFolder\cisl"))
+    if (!(Test-Path -Path "$tempFolder\mosl"))
     {
-        New-Item -Path "$tempFolder\cisl" -ItemType Directory | Out-Null
+        New-Item -Path "$tempFolder\mosl" -ItemType Directory | Out-Null
     }
     
-    $url = 'https://github.com/NikoNeugebauer/CISL/archive/master.zip'
+    $url = 'https://github.com/NikoNeugebauer/MOSL/archive/master.zip'
 
     $tempFolder = ([System.IO.Path]::GetTempPath()).TrimEnd("\")
-    $zipfile = "$tempFolder\cisl.zip"
+    $zipfile = "$tempFolder\mosl.zip"
 
 
     if (!(Test-Path -Path $path))
@@ -92,12 +92,12 @@ function Update-CISL(
 	$destinationFolder.CopyHere($zipPackage.Items())
 	
 	Write-Output "Cleaning up"
-	Move-Item -Path "$tempFolder\cisl-master\*" -Destination $path
-	Remove-Item -Path "$tempFolder\cisl-master"
+	Move-Item -Path "$tempFolder\mosl-master\*" -Destination $path
+	Remove-Item -Path "$tempFolder\mosl-master"
 	Remove-Item -Path $zipfile
 	
 	Write-Output "Done!"
-	if ((Get-Command -Module cisl).count -eq 0) { Import-Module "$path\cisl.psd1" -Force }
-	Get-Command -Module cisl
-	Write-Output "`n`nIf you experience any function missing errors after update, please restart PowerShell or reload your profile."
+	#if ((Get-Command -Module mosl).count -eq 0) { Import-Module "$path\mosl.psd1" -Force }
+	#Get-Command -Module mosl
+	#Write-Output "`n`nIf you experience any function missing errors after update, please restart PowerShell or reload your profile."
 }
